cb02b6bb53b0e210ac91314caa87b3bacc76099e
maverick <102207195+madhavsp1997@users.noreply.github.com>
2024-11-09 13:32:37 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"; // Import the App component
+
+// Render the app into the root element
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
